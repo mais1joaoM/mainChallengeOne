4e2741c7b18386788c791e662ac851e55be8c673
mais1joaoM <joao.saul0101@gmail.com>
2021-09-15 23:02:48 -0300

    Testes executados e concluidos com sucesso

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,45 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, data])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+
+    const UpdateTasks = tasks.map(task => ({...task}))
+
+    const foundItem = UpdateTasks.find(item => item.id === id)
+
+    if(!foundItem){
+      return;
+    }
+
+    foundItem.done = !foundItem.done;
+    setTasks(UpdateTasks)
+    
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    //TODO - remove task from stat
+
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
+
   }
 
   return (
